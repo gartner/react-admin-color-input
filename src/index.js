3c302b857e4bbd3b99b638b458e276948d210b63
Mads Lie Jensen <mads@gartneriet.dk>
2020-11-13 06:35:23 +0100

    Move LightenDarkenColor into own package, use from there

--- src/index.js
+++ src/index.js
@@ -8,38 +8,9 @@ import get from 'lodash.get';
 import pure from 'recompose/pure';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import {FocusWithin} from 'react-focus-within'
+import { LightenDarkenColor} from "@palustris/js-utils/lib/Color/LightenDarken";
 
 require('./ColorInput.css');
-
-const LightenDarkenColor = (col, amt) => {
-
-    let usePound = false;
-
-    if (col[0] == "#") {
-        col = col.slice(1);
-        usePound = true;
-    }
-
-    let num = parseInt(col,16);
-
-    let r = (num >> 16) + amt;
-
-    if (r > 255) r = 255;
-    else if  (r < 0) r = 0;
-
-    let b = ((num >> 8) & 0x00FF) + amt;
-
-    if (b > 255) b = 255;
-    else if  (b < 0) b = 0;
-
-    let g = (num & 0x0000FF) + amt;
-
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-
-    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-
-}
 
 const ColorFieldComponent = ({source, record = {}, className}) =>
     (
